8db2abff7fd3ff80e5ca73a41020d4a014faa501
matosalko <salko.matej@gmail.com>
2020-10-18 17:09:09 +0200

    rozbehanie elasticsearch, indexovanie

--- src/index.py
+++ src/index.py
@@ -1,23 +1,56 @@
 import re
+from elasticsearch import Elasticsearch
 import json
+import os
 
-with open('data/yago-wd-facts.nt') as file:
+
+class Record():
+
+    def __init__(self, sub_type, subject, relation, obj_type, object):
+        self.sub_type = sub_type
+        self.subject =  subject
+        self.relation = relation
+        self.obj_type = obj_type
+        self.object = object
+
+    def toJSON(self):
+        return self.__dict__
+
+
+def search(index):
+    res = es.search(index=index.lower(), body={"query": {"match_all": {}}})
     
-    for line in file:
-        result = re.findall('[^/]*/[^/]*>', line)
-        
-        subject = result[0].split('/')
-        relation = result[1].split('/')
-        object = result[2].split('/')
+    return res['hits']['hits']
 
-        item = {
-            "subj_type": subject[0],
-            "subject": subject[1][:-1],
-            "relation": relation[1][:-1],
-            "obj_type": object[0],
-            "object": object[1][:-1]
-        }
 
-        print(item)
-        break
+def index_file(file_name):
+    
+    with open(f"data/{file_name}", encoding='utf-8') as file:    
+        for count, line in enumerate(file):
+            results = re.findall('[^/]*/[^/]*>', line)
+            
+            subject = results[0].split('/')
+            relation = results[1].split('/')
+            object = results[2].split('/')
 
+            record = Record(subject[0], subject[1][:-1], relation[1][:-1], object[0], object[1][:-1])
+
+            index = record.subject.lower()
+            index = re.sub('[ \"*<|,>/?:]', '', index)  # odstrani znaky, ktore su zakazane v ES indexe
+            index = re.sub('_\(.*\)', '', index)    # odstrani doplnujucu informaciu z indexu napr. Goo_(album) bude len Goo
+
+            print(f"indexing #{count} {index}")
+
+            res = es.index(index=index, id=count, body=record.toJSON())
+            # res = es.delete(index=index, id=0)
+
+
+es = Elasticsearch()
+cwd = os.getcwd()
+all_files = os.listdir(f"{cwd}/data")
+
+index_file('yago-wd-facts.nt')
+
+# print(search('Goo_(album)'))
+
+# es.allocation_explain()

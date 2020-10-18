2efb3d5c0786cf47b9384fa5d921945899d3126b
matosalko <salko.matej@gmail.com>
2020-10-18 21:57:37 +0200

    fixed search

--- src/index.py
+++ src/index.py
@@ -5,23 +5,35 @@ import os
 
 class Record():
 
-    def __init__(self, sub_type, subject, relation, obj_type, object):
+    def __init__(self, sub_type, subject, relation, obj_type, object, name):
         self.sub_type = sub_type
         self.subject =  subject
         self.relation = relation
         self.obj_type = obj_type
         self.object = object
+    
+        self.name = name    # na zaklade tohto atributu sa bude dany zaznam vyhladavat
 
     def toJSON(self):
         return self.__dict__
 
 
 # vyhlada zadany retazec
-def search(index):
-
-    res = es.get(index='record_idx', id=index.lower())
+def search(record_name):
+    record_name = record_name.lower() 
+    record_name = re.sub('[ \"*<|,>/?:]', '', record_name)
     
-    return res
+    body = {
+        'query': {
+            'match': {
+                'name': record_name
+            }
+        }
+    }
+    res = es.search(index="record_idx", body=body)
+
+    return res['hits']['hits']
+
 
 
 # zaindexuje riadky zo suboru
@@ -35,13 +47,13 @@ def index_file(file_name):
             relation = results[1].split('/')
             object = results[2].split('/')
 
-            record = Record(subject[0], subject[1][:-1], relation[1][:-1], object[0], object[1][:-1])
-
-            id = record.subject.lower() 
+            id = subject[1][:-1].lower() 
             id = re.sub('[ \"*<|,>/?:]', '', id)  # odstrani znaky, ktore su zakazane v ES indexe
             id = re.sub('_\(.*\)', '', id)    # odstrani doplnujucu informaciu z indexu napr. Goo_(album) bude len Goo
 
-            es.index(index='record_idx', id=id, body=record.toJSON())
+            record = Record(subject[0], subject[1][:-1], relation[1][:-1], object[0], object[1][:-1], id)
+
+            es.index(index='record_idx', id=count, body=record.toJSON())
             # break
 
 
@@ -50,3 +62,5 @@ cwd = os.getcwd()
 all_files = os.listdir(f"{cwd}/data")
 
 index_file('yago-wd-facts.nt')
+
+# print(search('Tempest'))

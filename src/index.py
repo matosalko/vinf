74604d60dd1bcda56eb62c996caddf34e2932d50
matosalko <salko.matej@gmail.com>
2020-10-27 16:09:55 +0100

    indexing works for all files

--- src/index.py
+++ src/index.py
@@ -2,6 +2,8 @@ import re
 from elasticsearch import Elasticsearch
 import os
 
+from time import time
+
 
 class Record():
 
@@ -35,32 +37,51 @@ def search(record_name):
     return res['hits']['hits']
 
 
+def index_data(file_name, index_name):
 
-# zaindexuje riadky zo suboru
-def index_file(file_name):
-    
     with open(f"data/{file_name}", encoding='utf-8') as file:    
         for count, line in enumerate(file):
-            results = re.findall('[^/]*/[^/]*>', line)
             
+            if re.findall('_:', line):  # ignoruje riadky, ktore nie su reprezentovane ako N-triple
+                continue
+
+            line = re.sub('#', '/', line)
+            results = re.findall('[^/]*/[^/]*>', line)
+
             subject = results[0].split('/')
             relation = results[1].split('/')
-            object = results[2].split('/')
-
+            
             id = subject[1][:-1].lower() 
-            id = re.sub('[ \"*<|,>/?:]', '', id)  # odstrani znaky, ktore su zakazane v ES indexe
-            id = re.sub('_\(.*\)', '', id)    # odstrani doplnujucu informaciu z indexu napr. Goo_(album) bude len Goo
+            id = re.sub('[ \"\.*<|,>/?:]', '', id)  # odstrani nejake specialne znaky
+            id = re.sub('_\(.*\)', '', id)    # odstrani doplnujucu informaciu napr. Goo_(album) bude len Goo
 
-            record = Record(subject[0], subject[1][:-1], relation[1][:-1], object[0], object[1][:-1], id)
+            if len(results) == 3:
+                object = results[2].split('/')
+                record = Record(subject[0], subject[1][:-1], relation[1][:-1], object[0], object[1][:-1], id)
+            
+            else:
+                object = re.findall('\".*\"', line)
+                record = Record(subject[0], subject[1][:-1], relation[1][:-1], 'text', object[0], id)
+            
 
-            es.index(index='record_idx', id=count, body=record.toJSON())
-            # break
+            es.index(index=index_name, id=count, body=record.toJSON())
+            # print(record.toJSON())
+
 
 
 es = Elasticsearch()
 cwd = os.getcwd()
 all_files = os.listdir(f"{cwd}/data")
 
-index_file('yago-wd-facts.nt')
+for file in all_files:
+    print(f"indexing file: {file}")
 
-# print(search('Tempest'))
+    index_name = re.findall('[^-.*]*\.nt', file)
+    index_name = re.sub('.nt', '_idx', index_name[0])
+    index_name = index_name.lower()
+
+    start = time()
+    index_data(file, index_name)
+    end = time()
+
+    print(f"time needed for indexing: {end - start}")
